354eb3fcb20404daecbf32eb69669e421206cda8
stephen locas <flansmaster@163.com>
2022-11-05 11:16:35 +0800

    0

--- demo/fibonacci.c
+++ demo/fibonacci.c
@@ -3,6 +3,7 @@ int main () {
     long long num1,num2,num3,line;
     num1=1;
     num2=1;
+    
     printf("How many lines do you want?\n");
     scanf_s("%lld",&line);
     if(line==1) printf("%lld\n",num1);
