a791473513f2b72c2a520f0604661a9724cf7320
stephen locas <flansmaster@163.com>
2022-11-09 20:58:12 +0800

    changes

--- demo/a.c
+++ demo/a.c
@@ -1,42 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+/******************  begin *********************/
+/*实验4-3跟踪调试题程序利用R计算圆的面积s*/ 
+#include<stdio.h>
+#include <assert.h>
+#define  R
+int integer_fraction(float x);
+int main(void)
 {
+	float  r;
+   double s;
+	int s_integer=0;
+   scanf_s("%f",&r);
+   #ifdef  R
+      s=(3.14159)*(r);
+      s=(s)*(r);
+      printf("Area of round is: %lf\n",s);
+      s_integer=integer_fraction(s);
+      // assert((s-s_integer)<0.5);
+      printf("The integer fraction of area is %d\n", s_integer+1);
+   #endif
+   return 0;
+}
 
-    int a = 12;
-    printf("八进制 --> %o\n", a);
-    printf("十六进制 --> %X\n", a);
-    printf("十进制 --> %d\n", a);
-    char s[10];
-    itoa(a, s,2);
-    printf("二进制 --> %s\n", s);
-    itoa(a, s, 3);
-    printf("三进制 --> %s\n", s);
-
-
-
-
-    
-    return 0;
+int integer_fraction(float x)
+{
+  int i=x;
+  return i;
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/******************   end  *********************/
