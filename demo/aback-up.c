2779ea87e7dd3ee16721bcd745b87e9e05df7e9b
stephen locas <flansmaster@163.com>
2022-11-22 18:56:31 +0800

    new

--- demo/aback-up.c
+++ demo/aback-up.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main(){
-    int i;
-    srand(time(0));
-    for (i=1;i<=10;i++){
-        printf("%d\n",rand()%100);
+int main()
+{
+char ch1[100]={0};
+char ch2[100]={0};
+int i=0,c=0;
+int j=0;
+while((ch1[i]=getchar())!='\n') i++;
+while((ch2[j]=getchar())!='\n') j++;
+for(c=0;c<100;c++)
+{
+    if(ch1[c]=='\n')
+    {
+        for (int x=0;x<100;x++)
+        {
+            if(ch2[x]=='\n') break;
+            ch1[c]=ch2[x];
+            c++;
+        }
+        break;
     }
-    return 0;
+    continue;
+}
+printf("%s",ch1);
+return 0;
 }
