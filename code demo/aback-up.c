19455aa750eb53b114e53a0ab80457e31351cc5b
stephen locas <flansmaster@163.com>
2022-11-29 20:43:12 +0800

    up

--- code demo/aback-up.c
+++ code demo/aback-up.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
-void menu()
-{
-    printf("Please choose one of the following:\n"
-    "1)copy files\n2)move files\n3)remove files\n4)quit\n"
-    "Enter the number of your choice:\n");
-}
-int main_body(int a,int b)
-{
-    int c=0;
-    int flag;
-    flag=scanf("%d",&c);
-    if (flag==1&&c<=a&&c>=b) printf("%d",c);
-    else if(flag==0) printf("%d",4);
-    else
-    {
-        menu();
-        main_body(a,b);
-    }
-}
+#include<stdio.h>
+void sharp(char ch);
 int main()
 {
-    menu();
-    int a=44,b=3;
-    main_body(a,b);
-    return 0;
+    char line[10]={0};
+    int i=0;
+    while((line[i]=getchar())!='\n')
+    {
+        i++;
+    }
+    // gets(line);
+    for (int x=0;x<9;x++)
+    {
+        sharp(line[x]);
+        printf("%c",line[x]);
+    }
+
 }
+void sharp(char ch)
+{
+    if(ch>='0'&&ch<='9') printf("#");
+} 
