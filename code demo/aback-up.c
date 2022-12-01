ab435c1005007efb1227d4ded1a98a5cf747a0d8
stephen locas <flansmaster@163.com>
2022-12-01 16:19:24 +0800

    updated

--- code demo/aback-up.c
+++ code demo/aback-up.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
-void sharp(char ch);
+void FMax(int a[],int n,int* max,int* min);
 int main()
 {
-    char line[10]={0};
-    int i=0;
-    while((line[i]=getchar())!='\n')
-    {
-        i++;
-    }
-    // gets(line);
-    for (int x=0;x<9;x++)
-    {
-        sharp(line[x]);
-        printf("%c",line[x]);
-    }
-
+	int a[5],n=0;
+	for(n=0;n<5;n++)
+	{
+		scanf_s("%d",&a[n]);
+	}
+	int big=a[0],small=a[0];
+	FMax(a,n,&big,&small);
+	printf("Max:%d\n",big);
+	printf("Min:%d\n",small);
+	return 0;
+	
+	
 }
-void sharp(char ch)
+void FMax(int a[],int n,int* max,int* min)
 {
-    if(ch>='0'&&ch<='9') printf("#");
-} 
+	for (int i=0;i < n; i++)
+	{
+		if (*max<a[i]) 
+			// *max=a[i];
+			*max=*(a+i);
+		else if (*min>a[i]) 
+			// *min=a[i];
+			*min=*(a+i);
+	}
+}
